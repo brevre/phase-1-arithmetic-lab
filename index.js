84d90ccabbbff1b5c21bbfa2e9e9aa22f0f85256
brevre <brendamakena09@gmail.com>
2023-09-28 02:58:24 +0200

    completed

--- index.js
+++ index.js
@@ -1,2 +1,22 @@
 
 // Write your code here
+// Create a variable called multiply to multiply num1 and num2
+var num1 = 31;
+var  num2 = 2;
+var multiply = num1 * num2;
+
+// Create a variable called random to generate a random integer greater than 0
+var random = Math.ceil(Math.random() * 100);
+
+// Create a variable called mod to calculate the remainder of dividing num3 by num4
+var num3 = 34;
+var num4 = 30;
+var mod = num3 % num4;
+
+// Create a variable called max to find the highest number in a set
+var max = Math.max(3, 5, 10, 15, 20);
+
+console.log("multiply:", multiply); 
+console.log("random:", random);     
+console.log("mod:", mod);           
+console.log("max:", max);           
